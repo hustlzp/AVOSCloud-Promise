d96f9c691b30ba10567f321a9ea0de91abd4bc2d
hustlzp <hustlzp@qq.com>
2019-04-26 13:23:17 +0800

    First commit.

---
+++ Podfile
@@ -0,0 +1,11 @@
+source 'git@github.com:hustlzp/Specs.git'
+source 'https://github.com/CocoaPods/Specs.git'
+
+# Uncomment the next line to define a global platform for your project
+platform :ios, '10.0'
+
+
+target 'AVOSCloud-Promise-iOS' do
+    pod 'AVOSCloud', '11.6.2.102'
+    pod 'PromiseKit', '~> 6.8.4'
+end
---
+++ Sources/AVOSCloud+Promise.swift
@@ -0,0 +1,36 @@
+//
+//  AVOSCloud+Promise.swift
+//  
+//
+//  Created by hustlzp on 2019/4/26.
+//
+
+import Foundation
+import AVOSCloud
+import PromiseKit
+
+extension AVCloud {
+    class func callFunction(_: PMKNamespacer, name: String, parameters: [String: Any]?) -> Promise<Any?> {
+        return Promise {
+            callFunction(inBackground: name, withParameters: parameters, block: $0.resolve)
+        }
+    }
+    
+    class func rpcFunction<T>(_: PMKNamespacer, name: String, cachePolicy: AVCachePolicy, maxCacheAge: TimeInterval,b parameters: [String: Any?]?) -> Promise<T> {
+        return Promise { resolver in
+            rpcFunction(inBackground: name, withParameters: parameters, cachePolicy: cachePolicy, maxCacheAge: maxCacheAge, block: { (result, fromCache, error) in
+                guard error == nil else {
+                    resolver.reject(error!)
+                    return
+                }
+                
+                guard let object = result as? T else {
+                    resolver.reject(NSError(domain: kLeanCloudErrorDomain, code: kAVErrorIncorrectType, userInfo: [NSLocalizedDescriptionKey: "Return data's type should be \(T.self)"]))
+                    return
+                }
+                
+                resolver.fulfill(object)
+            })
+        }
+    }
+}
